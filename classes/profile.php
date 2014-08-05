89d30aa2b3d431b5ce30eef591fb3ab133f8b52e
masande1531 <masande@globalimsa.com>
2014-08-05 20:12:23 +0200

    Update profile.php

--- classes/profile.php
+++ classes/profile.php
@@ -3,40 +3,38 @@
 require_once 'database.php';
 
 class profile {
-
-    function view_profile($user_id) {
-        $str_sql_select = "SELECT *FROM users WHERE userID = ? ";
+   static function view_profile($user_id) {
+        $str_sql_select = "SELECT *FROM users WHERE username = ? ";
         $sql_paramter = array($user_id);
-
-        return( Database::Query($str_sql_select, $sql_paramter, TRUE));
+        return Database::Query($str_sql_select, $sql_paramter, TRUE);
+    }
+        
+    static function is_password_correct($pass, $user_id) {
+        
+       $str_sql_select = "SELECT password, userID FROM users WHERE password = ? AND userID = ?";
+        $sql_paramter = array($pass, $user_id);
+        Database::Query($str_sql_select, $sql_paramter, TRUE);
+        $row = Database::$int_num_rows;
+        foreach ($row as $rows) {
+            
+        }
+        if ( $rows == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
     
-    
-    function is_pass_correct($pass, $user_id) {
-
-        $encrypt = $pswd;
-        $isFound = false;
-
-        if ($row = mysql_fetch_array(mysql_query(" select * from users where userID = '$userID' "))) {
-            if ($encrypt == $row['password']) {
-                $isFound = true;
-            }
+    static function update_password($user_id, $pass) {
+        
+        $str_sql_select = "UPDATE users SET password = ? WHERE userID = ?";
+        $sql_paramter = array($pass, $user_id);
+        $upadate = Database::Query($str_sql_select, $sql_paramter, TRUE);
+        if ($upadate == true) {
+            return true;
+        } else {
+            return false;
         }
-
-        return $isFound;
-    }
-    
-    function change_password($pass, $user_id) {
-
-        $isUpdated = false;
-        $userPSWD = $password;
-
-        if (mysql_query(" update users set password = '$userPSWD' where userID= '$userID' ")) {
-            $isUpdated = true;
-        }
-
-
-        return $isUpdated;
     }
 
 }
